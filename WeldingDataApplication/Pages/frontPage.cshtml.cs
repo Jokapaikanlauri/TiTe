e2f6b98ff811e0bde35ae897467ed2050095451b
Sebastian-1995 <81906311+Sebastian-1995@users.noreply.github.com>
2023-01-19 18:02:53 +0200

    Project folder

---
+++ WeldingDataApplication/Pages/frontPage.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeldingDataApplication.Pages
+{
+    public class Index1Model : PageModel
+    {
+
+
+        public void OnGet()
+        {
+
+
+        }
+    }
+}
