3d3fd86412537952f50f76609c87e7a8bd3fb9be
villeniskanen <Ville.Niskanen2@edu.savonia.fi>
2023-02-01 10:15:22 +0200

    Weld luokka

---
+++ WeldingDataApplication/Classes/Weld.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace WeldingDataApplication.Classes
+{
+   /* public class Weld
+    {
+        public Weld()
+        {
+            private int Id { get; set; }
+            private DateTime Timestamp { get; set; }
+            private int ProcessingStepNumber { get; set; }
+            private int PartSerialNumber { get; set; }
+            private int PartArticleNumber { get; set; }
+            private string MachineType { get; set; }
+            private string MachineSerialNumber { get; set; }
+            private string Details { get; set; }
+            private string State { get; set; }
+            private string Welder { get; set; }
+
+        }
+
+
+	}*/
+
+    /*
+     * Tuleeko tämän
+     
+     */
+
+
+}
+
---
+++ WeldingDataApplication/ReadMe.txt
@@ -0,0 +1,19 @@
+﻿
+
+
+http://weldcube.ky.local 
+kt: R1_K2023 
+Ss: J0hae38#
+
+
+
+WeldCube API Key
+dc55e8bbc6b73dbb17c5ecf360a0aeb1
+
+
+
+http://weldcube.ky.local/api/v4/Welds
+
+GET /api/v4/Welds/{weldId} Gets the weld details
+GET /api/v4/Welds/{weldId}/Sections/{sectionNumber} Gets the section details
+GET /api/v4/Welds/{weldId}/ActualValues Gets the actual values of a weld
