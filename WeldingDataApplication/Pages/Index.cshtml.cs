935d5d1ea74e73b547bb10cb6f11eee03ce55885
Sebastian-1995 <81906311+Sebastian-1995@users.noreply.github.com>
2023-02-08 17:26:52 +0200

    Http test

--- WeldingDataApplication/Pages/Index.cshtml.cs
+++ WeldingDataApplication/Pages/Index.cshtml.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WeldingDataApplication.Classes;
 
 namespace WeldingDataApplication.Pages
 {
     public class IndexModel : PageModel
     {
         static HttpClient myHttpClient = new HttpClient();
-        public string Message { get; set; }
+        public List<Weld>? Message;
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -16,9 +17,9 @@ namespace WeldingDataApplication.Pages
 
         public async Task OnGetAsync()
         {
-            Message = myHttpClient.GetAsync("http://weldcube.ky.local/api/v4/Welds?api_key=dc55e8bbc6b73dbb17c5ecf360a0aeb1%20").ToString();
-
+            Message = await myHttpClient.GetFromJsonAsync<List<Weld>>("http://weldcube.ky.local/api/v4/Welds?api_key=dc55e8bbc6b73dbb17c5ecf360a0aeb1%20");
         }
+
         public void OnGet()
         {
             
