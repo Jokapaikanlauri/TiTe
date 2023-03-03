07e5a6f49189b5465ebb17fdad5adc7796b81c56
villeniskanen <Ville.Niskanen2@edu.savonia.fi>
2023-03-03 15:08:36 +0200

    json write ja read

--- WeldingDataApplication/Pages/Index.cshtml.cs
+++ WeldingDataApplication/Pages/Index.cshtml.cs
@@ -10,6 +10,12 @@ using WeldingDataApplication.Classes;
 using System.Text.Json;
 using System.Net.NetworkInformation;
 using System.Text.Json.Serialization;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+
 
 namespace WeldingDataApplication.Pages
 {
@@ -18,13 +24,14 @@ namespace WeldingDataApplication.Pages
         static HttpClient myHttpClient = new HttpClient();
         public Weld.Root? Message;
         public List<WeldDetails.RootObject>? rootList;
+
         public List<Database>? DatabaseItems = new List<Database>();
         public List<Database>? SuccessList = new List<Database>();
         public List<Database>? ErrorsList = new List<Database>();
         private readonly ILogger<IndexModel> _logger;
         private string apiKey = "?api_key=dc55e8bbc6b73dbb17c5ecf360a0aeb1";
+        public string filePath = "./WeldData.json";
 
-        
         static string apiEndpoint = "https://api.courier.com/send";
         static string token = "Bearer " + "dk_prod_0N0HT7KZK64JPHHD1CXY0PKQBF5K";
 
@@ -77,37 +84,59 @@ namespace WeldingDataApplication.Pages
                 myHttpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
                 // Haetaan rajapinnasta 
                 Message = await myHttpClient.GetFromJsonAsync<Weld.Root>("http://weldcube.ky.local/api/v4/Welds?api_key=dc55e8bbc6b73dbb17c5ecf360a0aeb1%20");
-       
+
+
+               string WeldData =  System.IO.File.ReadAllText("./WeldData.json");
+               List <Database> data = JsonConvert.DeserializeObject<List<Database>>(WeldData);
+
+                foreach (var weldItem in data)
+                {
+                   // Console.WriteLine(weldItem.Row);
+                }
+
+
+
 
                 var i = 0;
                 var o = 0;
                 var a = 0;
-                StreamWriter sw = new StreamWriter("/Users/villeniskanen/Desktop/Tietotekniikkaproj/group-1/WeldingDataApplication/AllWelds.txt");
+                
                 
                 foreach (Weld.WeldInfo item in Message.WeldInfos)
                 {
                     var url = item.Details;
                     url += apiKey;
-                    // Kirjoitetaan txt tiedostoon kaikki nyt olemassa olevat tiedostot
-                    
-                    /*   //Pass the file path and file name to the StreamReader constructor
-                        StreamReader sr = new StreamReader("C:\\Sample.txt");
-                        //Read the first line of text
-                        line = sr.ReadLine();
-                        //Continue to read until you reach end of file
-                        while (line != null)
-                        {
-                            //write the line to console window
-                            Console.WriteLine(line);
-                            //Read the next line
-                            line = sr.ReadLine();
-                        }
-                        //close the file
-                        sr.Close();*/
 
                     //Frontendin lokit.
+                    if (item.State != "Ok")
+                    {
+                        // Haetaan detailit vain jossa on error olemassa
+                        var rootWanted = await myHttpClient.GetFromJsonAsync<WeldDetails.RootObject>(url);
 
-                    var rootWanted = await myHttpClient.GetFromJsonAsync<WeldDetails.RootObject>(url);
+
+                        foreach (var violation in rootWanted.WeldData.LimitViolations)
+                        {
+                            string valueType = violation.ValueType;
+                            string violationType = violation.ViolationType;
+                            // Jos sama id nii ei luoda id:lle uutta rivia vaan lisätään id:hen violation objectit
+
+                            o++;
+                            // Hitsaukset jossa on error
+                            ErrorsList.Add(new Database
+                            {
+                                Row = o,
+                                Id = item.Id,
+                                ViolationType = violation.ViolationType,
+                                Valuetype = violation.ValueType,
+                                State = item.State,
+                                Time = item.Timestamp.ToShortTimeString(),
+                                Date = item.Timestamp.ToShortDateString(),
+                                TimeStamp = item.Timestamp
+                            });
+                            //await sendEmail();
+                        }
+                    }
+                  
 
                     a++;
                     DatabaseItems.Add(new Database
@@ -166,34 +195,15 @@ namespace WeldingDataApplication.Pages
                     }
 
                     // täältä tulee hits id jolla voi olla paaljon erroreita
-
-                    foreach (var violation in rootWanted.WeldData.LimitViolations)
-                    {
-                        string valueType = violation.ValueType;
-                        string violationType = violation.ViolationType;
-                        // Jos sama id nii ei luoda id:lle uutta rivia vaan lisätään id:hen violation objectit
-
-                        o++;
-                        // Hitsaukset jossa on error
-                        ErrorsList.Add(new Database
-                        {
-                            Row = o,
-                            Id = item.Id,
-                            ViolationType = violation.ViolationType,
-                            Valuetype = violation.ValueType,
-                            State = item.State,
-                            Time = item.Timestamp.ToShortTimeString(),
-                            Date = item.Timestamp.ToShortDateString(),
-                            TimeStamp = item.Timestamp
-                        }) ;
-                        //await sendEmail();
-                    }
+             
                 }
-
+                
                 // Suljetaan kirjottaminen
+                string jsonString = JsonConvert.SerializeObject(DatabaseItems, Formatting.Indented);
+                byte[] bytes = Encoding.UTF8.GetBytes(jsonString);
+                System.IO.File.WriteAllBytes(filePath, bytes);
        
-                sw.Close();
-
+          
             }
             catch (Exception e)
             {
